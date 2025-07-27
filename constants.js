80b7a6a11069296322b566dae3fdbd4e60c30898
binnesman <92jerrypowers@gmail.com>
2025-07-27 07:00:30 -0500

    Update patch notes and addon list path

--- constants.js
+++ constants.js
@@ -7,6 +7,6 @@ module.exports = {
   MAGNET_LINK: 'magnet:?xt=urn:btih:2ba2833baf733ce0a16040d43ed09491f2bf2ab2&dn=ChromieCraft_3.3.5a.zip&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=http%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.uw0.xyz%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.zerobytes.xyz%3A1337%2Fannounce',
   CLIENT_ZIP_FILE: 'ChromieCraft_3.3.5a.zip',
   WEB_URI: 'https://www.synastria.org/',
-  PATCH_NOTES_URL: "https://raw.githubusercontent.com/danhicks853/SynLauncher/refs/heads/main/patch_notes.html",
-  ADDONS_LIST_URL: "https://raw.githubusercontent.com/danhicks853/SynLauncher/refs/heads/main/addons_list.json"
+  PATCH_NOTES_URL: "https://raw.githubusercontent.com/binnesman/SynLauncher/refs/heads/main/patch_notes.html",
+  ADDONS_LIST_URL: "https://raw.githubusercontent.com/binnesman/SynLauncher/refs/heads/main/addons_list.json"
 };
